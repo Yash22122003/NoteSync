3d2339f16e509e209d01c91f64ef78871d26c013
yash-git-hub <yash2003agrahari@gmail.com>
2026-09-03 23:13:43 +0530

    Day 2: Add ORM and note listing

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+
+def note_list(request):
+    notes = Note.objects.all()
+
+    return render(request, "notes/note_list.html", {
+        "notes": notes
+    })
