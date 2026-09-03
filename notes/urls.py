0703c00a8392a4c3c136cece4ec5bce6c50928f9
yash-git-hub <yash2003agrahari@gmail.com>
2026-09-04 00:47:53 +0530

    Day 2: Add note create update and delete

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.note_list, name='note_list'),
+    path('create/', views.note_create, name='note_create'),
+    path('edit/<int:id>/', views.note_edit, name='note_edit'),
+    path('delete/<int:id>/', views.note_delete, name='note_delete'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 
 
@@ -8,3 +8,38 @@ def note_list(request):
     return render(request, "notes/note_list.html", {
         "notes": notes
     })
+
+def note_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        Note.objects.create(
+            title=title,
+            content=content
+            )
+        return redirect("note_list")
+    return render(request, "notes/note_create.html")
+
+def note_edit(request, id):
+    note = Note.objects.get(id=id)
+
+    if request.method == "POST":
+        note.title = request.POST["title"]
+        note.content = request.POST["content"]
+
+        note.save()
+
+        return redirect("note_list")
+
+    return render(request, "notes/note_edit.html", {
+        "note": note
+    })
+
+
+def note_delete(request, id):
+    note = Note.objects.get(id=id)
+
+    note.delete()
+
+    return redirect("note_list")
